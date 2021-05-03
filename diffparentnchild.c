212d0044d86a795e424088cccb5713da3df3969f
lehamsyahmin <ilham.syahmin99@gmail.com>
2021-05-03 13:31:51 +0000

    loopCoding

--- diffparentnchild.c
+++ diffparentnchild.c
@@ -1,6 +1,7 @@
 #include <stdlib.h> /*needed to define exit()*/
 #include <unistd.h> /*needed for fork() and getpit()*/
 #include <stdio.h> /*needed for printf()*/
+#include <sys/wait.h>
 
 int main(int arg, char**argv){
 	int pid;  /*process ID*/
@@ -12,6 +13,7 @@ int main(int arg, char**argv){
 		break;
 
 	default: /*a fork returns a pid to the parent*/
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 
